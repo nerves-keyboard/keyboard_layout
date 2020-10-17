5f98f03265e33f96a129ed420d4f2ada1e633e6a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-10-16 23:47:25 -0700

    add credo, coveralls, dialyzer

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule KeyboardLayout.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :keyboard_layout,
-      version: "0.1.0",
-      elixir: "~> 1.11",
+      version: @version,
+      elixir: "~> 1.10",
+      deps: deps(),
+      description: description(),
+      dialyzer: dialyzer(),
+      preferred_cli_env: preferred_cli_env(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -18,6 +24,34 @@ defmodule KeyboardLayout.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13", only: :test, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Functions for using and defining keyboard layouts with keys and LEDs
+    """
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "_build/#{Mix.env()}",
+      flags: [:unmatched_returns, :error_handling, :underspecs]
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test,
+      "coveralls.json": :test,
+      credo: :test
+    ]
   end
 end
