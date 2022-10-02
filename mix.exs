7cb09b675fa5e806ae4728efe5292d53431742b7
Chris Dosé <chris@xn--dos-dma.com>
2022-10-02 16:03:18 -0700

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule KeyboardLayout.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/nerves-keyboard/keyboard_layout"
 
   def project do
